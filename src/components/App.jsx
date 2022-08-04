75e854b4ad6665660eec23d8de1f03590f98dbd8
Roman Radakhevych <akrir@ukr.net>
2022-08-04 14:55:24 +0300

    div

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import { Recipe } from "./Recipe/Recipe";
 import recipes from "../recepies.json";
 export const App = () => {
   return (
-    <div>
+    <>
       {recipes.map((recipe, idx) =>
         (<Recipe key={idx} recipe={recipe} />))
       }
-    </div>
+    </>
   );
 };
